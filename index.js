9fc4231a388c1bf8bb3f83c8df4af2171d53c075
Michajuu <73675435+Michajuu@users.noreply.github.com>
2021-04-21 09:09:16 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function Oblicz()
+{
+    let F = document.getElementById("F").value;
+    let Δt = document.getElementById("Δt").value;
+let a = document.getElementById("Φ");
+a.innerHTML = Δt * F;
+}
